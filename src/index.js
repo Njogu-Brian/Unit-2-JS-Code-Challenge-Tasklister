b1cc2c241e6ae0d7382b13ff6e1e3d11b4ce64e9
BrianNjogu <briannjogu85@gmail.com>
2024-10-28 08:58:54 +0300

    complete labs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
-
-  const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
+  const form = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskListElement = document.getElementById("list");
 
   // Add Form Behavior Here!
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
+    const taskText = taskInput.value;
+    if (taskText) {
+      const listItem = document.createElement("li");
+      listItem.className = "task-item";
+      listItem.innerText = taskText;
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerText = "Delete";
+      deleteBtn.className = "delete-btn";
+      deleteBtn.addEventListener("click", () => listItem.remove());
+
+      listItem.appendChild(deleteBtn);
+
+      taskListElement.appendChild(listItem);
+
+      taskInput.value = "";
+    }
+  });
   renderApp()
 });
